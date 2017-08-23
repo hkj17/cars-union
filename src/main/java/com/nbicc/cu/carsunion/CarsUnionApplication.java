fa934263d705cc29969cdbc702d2a8f406867226
bigmao <zjufd2013@gmail.com>
2017-08-23 16:31:11 +0800

    add druid

--- src/main/java/com/nbicc/cu/carsunion/CarsUnionApplication.java
+++ src/main/java/com/nbicc/cu/carsunion/CarsUnionApplication.java
@@ -2,8 +2,10 @@ package com.nbicc.cu.carsunion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class CarsUnionApplication {
 
 	public static void main(String[] args) {
