e38d43322ba62426fdd37e35de5b35d17627bb33
bigmao <zjufd2013@gmail.com>
2017-08-25 09:34:48 +0800

    增加 统一异常处理

---
+++ src/main/java/com/nbicc/cu/carsunion/configuration/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.nbicc.cu.carsunion.configuration;
+
+import com.nbicc.cu.carsunion.model.ErrorInfo;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by bigmao on 2017/8/25.
+ * 统一异常处理
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    //以后可以细分异常，各个处理
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public ErrorInfo<String> jsonErrorHandler(HttpServletRequest req, Exception e) {
+        ErrorInfo<String> r = new ErrorInfo<>();
+        r.setCode(ErrorInfo.ERROR);
+        r.setMessage(e.getMessage());
+        r.setUrl(req.getRequestURL().toString());
+        r.setData("Exception!");
+        return r;
+    }
+}
